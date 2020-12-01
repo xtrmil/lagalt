13dba9a327d2485293ff39fbfa7ebc3ff7e9ead9
Johnny Hoang <johnnyhoaang@hotmail.com>
2020-12-01 14:34:00 +0100

    chore: Add eslint and prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import Navbar from './utils/Navbar';
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import {Router, Route, Switch } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import { Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserHistory } from 'history';
 
@@ -10,10 +11,10 @@ const history = createBrowserHistory();
 function App() {
   return (
     <Router history={history}>
-      <Navbar history={history}/>
+      <Navbar history={history} />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path='/login' component={LoginPage}/>
+        <Route exact path="/login" component={LoginPage} />
       </Switch>
     </Router>
   );
