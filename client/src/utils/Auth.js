1a9581ee182cb9fdfd7586fb3a098fa2f62dabd5
Bumpfel <eric.enoksson@gmail.com>
2021-01-07 17:32:15 +0100

    fix(Auth): solution to prompt window closing early on register

--- client/src/utils/Auth.js
+++ client/src/utils/Auth.js
@@ -145,6 +145,7 @@ export const logout = async () => {
 };
 
 // non exports
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 const thirdPartyAuth = async (provider) => {
   try {
@@ -155,7 +156,7 @@ const thirdPartyAuth = async (provider) => {
       if (dev.mode !== DevMode.off) {
         phoneNumber = testPhoneNr;
       } else {
-        console.log('');
+        await delay(1000);
         phoneNumber = prompt(
           'Please enter your phone number to verify your identity' +
             '\n\nProclaimer: Google stores and uses phone numbers to improve spam and abuse prevention across all Google services. Standard rates may apply',
